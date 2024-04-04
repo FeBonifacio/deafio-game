b3527d17f510859ffb16b24ef24c3bf0ae09dd66
Fernando <fe.bonifacio@hotmail.com>
2024-04-03 22:27:54 -0300

    feat: validation with jqury created

--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,7 +1,48 @@
+import styled from 'styled-components';
+import useCreateGame from '../../services/useCreate';
+import { useFormValidation } from '../../utils/useFormValidation.js'; // Correção no import
+
+const ContainerDiv = styled.div`
+    padding: 20px;
+`;
+
 const CreateGamePage = () => {
+    const { formData, handleChange, handleSubmit } = useCreateGame();
+    
+    useFormValidation(handleSubmit); 
+    
     return (
-        <div>OI</div>
+        <ContainerDiv className="container">
+            <h1 className="font-weight-bold" style={{ fontFamily: 'Roboto', fontSize: '3rem' }}>Criar Novo Jogo</h1>
+            <form id="create-game-form" onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="nome" className="form-label">Nome:</label>
+                    <input type="text" className="form-control" id="nome" name="nome" value={formData.nome} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="descricao" className="form-label">Descrição:</label>
+                    <input className="form-control" id="descricao" name="descricao" style={{ resize: 'none' }}  value={formData.descricao} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="produtora" className="form-label">Produtora:</label>
+                    <input type="text" className="form-control" id="produtora" name="produtora" value={formData.produtora} onChange={handleChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="ano" className="form-label">Ano:</label>
+                    <input type="number" className="form-control" id="ano" name="ano" value={formData.ano} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="idadeMinima" className="form-label">Idade Mínima:</label>
+                    <input type="number" className="form-control" id="idadeMinima" name="idadeMinima" value={formData.idadeMinima} onChange={handleChange} />
+                </div>
+                <div className="container d-flex justify-content-center align-items-center" >
+                    <button type="submit" className="btn btn-primary" style={{ width: '200px', height: '50px' }}>
+                        Criar Jogo
+                    </button>
+                </div>
+            </form>
+        </ContainerDiv>
     );
-}
+};
 
 export default CreateGamePage;
---
+++ frontend/src/services/useCreate.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useCreateGame = () => {
+    const [formData, setFormData] = useState({
+        nome: '',
+        descricao: '',
+        produtora: '',
+        ano: '',
+        idadeMinima: '',
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+        try {
+            const response = await axios.post('http://localhost:3000/', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao criar o jogo:', error);
+        }
+    };
+
+    return {
+        formData,
+        handleChange,
+        handleSubmit,
+    }
+};
+
+export default useCreateGame;
